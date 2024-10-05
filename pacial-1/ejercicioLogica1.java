43bb90cdeff74a33e25dae3f53b446a77153fb36
darwingg777 <darwingarmaria@gmail.com>
2024-10-05 09:13:30 -0500

    Add files via upload

---
+++ pacial-1/ejercicioLogica1.java
@@ -0,0 +1,16 @@
+package parcial1;
+
+
+
+public class ejercicioLogica1 {
+    public static void main(String[] args) {
+        int contador=0;
+        int numero=3456;
+        for (int i = 0; i < 5; i++) {
+            contador=i; 
+        }
+        System.out.println("la cantidad de caracteres que hay en: "+numero+" es "+contador);
+
+        
+    }
+}
---
+++ pacial-1/ejercicioLogica2.java
@@ -0,0 +1,15 @@
+package parcial1;
+
+public class ejercicioLogica2 {
+    public static void main(String[] args) {
+        
+   
+    int contador = 0;
+    String palabra= "arbol";
+    for(int i = 0;i<palabra.length();i++)
+    {
+        contador = i;
+    }
+    System.out.println("la cantidad de caracteres que hay en: "+palabra+" es "+contador);
+  }
+}
